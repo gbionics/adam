8ae9e950f4461d42ac2287088dc27545f4074dad
giulero <gl.giuseppelerario@gmail.com>
2026-02-23 15:04:28 +0100

    Add USD model conversion functions to the exports

--- src/adam/model/conversions/__init__.py
+++ src/adam/model/conversions/__init__.py
@@ -1,1 +1,4 @@
 from .idyntree import to_idyntree_model
+from .usd import model_to_usd, model_to_usd_stage, urdf_to_usd
+
+__all__ = ["to_idyntree_model", "model_to_usd_stage", "model_to_usd", "urdf_to_usd"]
